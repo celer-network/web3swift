868eaad7c445a5a5be70186a05215f5845225299
Shvier <Shvier1894@gmail.com>
2020-07-02 22:22:23 +0800

    use custom SipHash

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/Shvier/SipHash.git", .revision("8833a6a5aae5ab05e034c475bd683ccdc74aeffa")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
